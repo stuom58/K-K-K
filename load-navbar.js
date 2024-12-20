d3a5a84b41338e6a10ca965b4c5b991489793901
stuom58 <stuom58@gmail.com>
2024-12-21 01:57:01 +0400

    load-navbar.js

--- load-navbar.js
+++ load-navbar.js
@@ -1,5 +1,5 @@
 function loadNavbar() {
-  fetch("../component/navbar.html")
+  fetch("navbar.html")
     .then((response) => response.text())
     .then((data) => {
       const navbarContainer = document.getElementById("navbar-container");
