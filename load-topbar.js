c19292ac9bfa1243dab2f171346b1016505cf84c
stuom58 <stuom58@gmail.com>
2024-12-21 01:57:25 +0400

    load-topbar.js

--- load-topbar.js
+++ load-topbar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('./../component/topbar.html')
+    fetch('./topbar.html')
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data); // افزودن نوار بالا به ابتدای بدنه
