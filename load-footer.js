fd96b51bb62b4136f75d939a918897b6e978cc01
stuom58 <stuom58@gmail.com>
2024-12-21 18:56:07 +0400

    new_3k

--- load-footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('./load-footer.html') // مسیر صحیح به فایل footer.html
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
-});
--- load-topbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('./topbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data); // افزودن نوار بالا به ابتدای بدنه
-        });
-});
